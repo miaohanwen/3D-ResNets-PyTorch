722222c407bfa6b2446237376cd0001743ac8ea7
Hanwen Miao <hwmiao@clarity31.eecs.umich.edu>
2021-01-06 02:04:06 -0500

    inference file for inference with only videos

--- inference.py
+++ inference.py
@@ -11,7 +11,8 @@ from utils import AverageMeter
 def get_video_results(outputs, class_names, output_topk):
     sorted_scores, locs = torch.topk(outputs,
                                      k=min(output_topk, len(class_names)))
-
+    print(sorted_scores)
+#     print(locs)
     video_results = []
     for i in range(sorted_scores.size(0)):
         video_results.append({
@@ -30,23 +31,22 @@ def inference(data_loader, model, result_path, class_names, no_average,
 
     batch_time = AverageMeter()
     data_time = AverageMeter()
-    results = {'results': defaultdict(list)}
+#     results = {'results': defaultdict(list)}
 
     end_time = time.time()
 
     with torch.no_grad():
-        for i, (inputs, targets) in enumerate(data_loader):
+        for i, inputs in enumerate(data_loader):
             data_time.update(time.time() - end_time)
-
-            video_ids, segments = zip(*targets)
+            print(inputs.shape)
             outputs = model(inputs)
+            print(outputs.shape)
             outputs = F.softmax(outputs, dim=1).cpu()
-
-            for j in range(outputs.size(0)):
-                results['results'][video_ids[j]].append({
-                    'segment': segments[j],
-                    'output': outputs[j]
-                })
+#             for j in range(outputs.size(0)):
+#                 results['results'][video_ids[j]].append({
+#                     'segment': segments[j],
+#                     'output': outputs[j]
+#                 })
 
             batch_time.update(time.time() - end_time)
             end_time = time.time()
@@ -58,28 +58,31 @@ def inference(data_loader, model, result_path, class_names, no_average,
                       len(data_loader),
                       batch_time=batch_time,
                       data_time=data_time))
+            average_scores = torch.mean(outputs, dim=0)
+            result = get_video_results(average_scores, class_names, output_topk)
+            print(result)
+    
+#     inference_results = {'results': {}}
+#     if not no_average:
+#         for video_id, video_results in results['results'].items():
+#             video_outputs = [
+#                 segment_result['output'] for segment_result in video_results
+#             ]
+#             video_outputs = torch.stack(video_outputs)
+#             average_scores = torch.mean(video_outputs, dim=0)
+#             inference_results['results'][video_id] = get_video_results(
+#                 average_scores, class_names, output_topk)
+#     else:
+#         for video_id, video_results in results['results'].items():
+#             inference_results['results'][video_id] = []
+#             for segment_result in video_results:
+#                 segment = segment_result['segment']
+#                 result = get_video_results(segment_result['output'],
+#                                            class_names, output_topk)
+#                 inference_results['results'][video_id].append({
+#                     'segment': segment,
+#                     'result': result
+#                 })
 
-    inference_results = {'results': {}}
-    if not no_average:
-        for video_id, video_results in results['results'].items():
-            video_outputs = [
-                segment_result['output'] for segment_result in video_results
-            ]
-            video_outputs = torch.stack(video_outputs)
-            average_scores = torch.mean(video_outputs, dim=0)
-            inference_results['results'][video_id] = get_video_results(
-                average_scores, class_names, output_topk)
-    else:
-        for video_id, video_results in results['results'].items():
-            inference_results['results'][video_id] = []
-            for segment_result in video_results:
-                segment = segment_result['segment']
-                result = get_video_results(segment_result['output'],
-                                           class_names, output_topk)
-                inference_results['results'][video_id].append({
-                    'segment': segment,
-                    'result': result
-                })
-
-    with result_path.open('w') as f:
-        json.dump(inference_results, f)
+#     with result_path.open('w') as f:
+#         json.dump(inference_results, f)
